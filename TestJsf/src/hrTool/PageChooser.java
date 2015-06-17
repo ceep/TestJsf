626ea4d2115d5d902c5f9c01d214a0097c1bfd4f
ceep <ceeprian@gmail.com>
2015-06-17 09:58:24 +0300

    Initial project.

---
+++ TestJsf/src/hrTool/PageChooser.java
@@ -0,0 +1,35 @@
+package hrTool;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+
+
+@ManagedBean
+@ApplicationScoped
+public class PageChooser implements Serializable {
+
+    private String page;
+
+    public String getPage() {
+    	if(page==null) page = "Dashboard";
+    	System.out.println("PageChooser GET with page link: " + page);
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+
+	@PostConstruct
+    public void init() {
+        
+        System.out.println("PageChooser INIT with page set to: " + page);
+    }
+
+    
+}
---
+++ TestJsf/src/hrTool/Team.java
@@ -0,0 +1,58 @@
+package hrTool;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Team implements Serializable{
+	
+	private String name;
+	private int numberOfEmployees;
+	private int numberOfEmployeesOnVacation;
+	private Date formedDate;
+	private int id;
+	private int companyId;
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(int companyId) {
+		this.companyId = companyId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+	public void setNumberOfEmployees(int numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+	public int getNumberOfEmployeesOnVacation() {
+		return numberOfEmployeesOnVacation;
+	}
+	public void setNumberOfEmployeesOnVacation(int numberOfEmployeesOnVacation) {
+		this.numberOfEmployeesOnVacation = numberOfEmployeesOnVacation;
+	}
+	public Date getFormedDate() {
+		if(formedDate==null) return new Date();
+		return formedDate;
+	}
+	public void setFormedDate(Date formedDate) {
+		this.formedDate = formedDate;
+	}
+	
+	
+	
+}
